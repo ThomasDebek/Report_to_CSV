3e733c59d68bda0fd675d7a479f5481bbbf09568
tom.debek@gmail.com <tom.debek@gmail.com>
2017-12-12 15:25:46 +0100

    Importing product Report to CSV

---
+++ app/models/product.rb
@@ -0,0 +1,23 @@
+class Product < ApplicationRecord
+
+
+
+  #def self.import(file)
+   # CSV.foreach(file.path, headers: true) do |row|
+    #  Product.create! row.to_hash
+    #end
+  #end
+
+
+
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |product|
+        csv << product.attributes.values_at(*column_names)
+      end
+    end
+  end
+
+
+end
